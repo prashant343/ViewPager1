781679387f0a5b455bbdbf2311956e2494582b36
prashant kaushik <prashant@ovalelabs.com>
2017-08-20 16:29:42 +0530

    first

---
+++ app/src/main/java/iotapps/viewpager1/Homefragment.java
@@ -0,0 +1,10 @@
+package iotapps.viewpager1;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by prashantkaushik on 13/03/17.
+ */
+
+public class Homefragment extends Fragment {
+}
---
+++ app/src/main/java/iotapps/viewpager1/Trendingfragment.java
@@ -0,0 +1,11 @@
+package iotapps.viewpager1;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by prashantkaushik on 13/03/17.
+ */
+
+public class Trendingfragment extends Fragment {
+
+}
